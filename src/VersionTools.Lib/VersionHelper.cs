22da8996dc35bc12d08c63cfc6d73724d435d593
David Tellander <dtellander@gmail.com>
2013-09-21 16:06:42 +0200

    Added testproject and implemented getting of file version and assembly version

---
+++ src/VersionTools.Lib/VersionHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace VersionTools.Lib {
+    public static class VersionHelper {
+        /// <summary>
+        /// Gets the file version of the calling assembly when no type is passed as an argument. 
+        /// When a type is passed, that type's containing version is returned.
+        /// </summary>
+        public static string GetAssemblyFileVersion(Type type = null) {
+            var assembly = type != null ? type.Assembly : Assembly.GetCallingAssembly();
+            var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return versionInfo.FileVersion;
+        }
+
+        /// <summary>
+        /// Gets the assembly version of the assembly. When no type is passed, the version of 
+        /// the calling assembly is used. Otherwise the version of the assembly that contains 
+        /// the type is returned.
+        /// </summary>
+        public static string GetAssemblyVersion(Type type = null) {
+            var assembly = type != null ? type.Assembly : Assembly.GetCallingAssembly();
+            var assemblyVersion = assembly.GetName().Version.ToString();
+            return assemblyVersion;
+        }
+    }
+}
---
+++ test/VersionTools.Lib.Test/VersionHelperTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3-beta2+master.deadb33f")]
+[assembly: AssemblyInformationalVersion("Test project v1.2.3")]
+
+namespace VersionTools.Lib.Test {
+    public class VersionHelperTest {
+        public class GetFileVersion_method {
+            [Fact]
+            public void should_return_the_full_file_version_string_of_the_calling_assembly() {
+                var fileVersion = VersionHelper.GetFileVersion();
+                Assert.Equal( "1.2.3-beta2+master.deadb33f", fileVersion );
+            }
+
+
+            [Fact]
+            public void should_return_the_full_file_version_of_the_assembly_that_contains_the_specified_type() {
+                var fileVersion = VersionHelper.GetFileVersion(typeof (VersionHelper));
+                Assert.Equal("1.0.0.0", fileVersion);
+            }
+        }
+
+        public class GetAssemblyVersion_method {
+            [Fact]
+            public void should_return_the_assembly_version_of_the_calling_assembly() {
+                var assemblyVersion = VersionHelper.GetAssemblyVersion();
+                Assert.Equal("1.2.3.0", assemblyVersion);
+            }
+
+
+            [Fact]
+            public void should_return_the_assembly_version_of_the_assembly_containing_the_specified_type() {
+                var assemblyVersion = VersionHelper.GetAssemblyVersion(typeof (VersionHelper));
+                Assert.Equal("1.0.0.0", assemblyVersion);
+            }
+
+        }
+    }
+}
