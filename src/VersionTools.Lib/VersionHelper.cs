7cb483c02342301d3b00894950d3f9848e4255e7
David Tellander <dtellander@gmail.com>
2013-09-21 16:59:35 +0200

    Added getting of product version of assembly

--- src/VersionTools.Lib/VersionHelper.cs
+++ src/VersionTools.Lib/VersionHelper.cs
@@ -10,7 +10,14 @@ namespace VersionTools.Lib {
         /// </summary>
         public static string GetAssemblyFileVersion(Type type = null) {
             var assembly = type != null ? type.Assembly : Assembly.GetCallingAssembly();
-            var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return GetAssemblyFileVersion(assembly.Location);
+        }
+
+        /// <summary>
+        /// Gets the file version of the assembly at the specified location.
+        /// </summary>
+        public static string GetAssemblyFileVersion(string location) {
+            var versionInfo = FileVersionInfo.GetVersionInfo(location);
             return versionInfo.FileVersion;
         }
 
@@ -23,6 +30,31 @@ namespace VersionTools.Lib {
             var assembly = type != null ? type.Assembly : Assembly.GetCallingAssembly();
             var assemblyVersion = assembly.GetName().Version.ToString();
             return assemblyVersion;
+        }
+
+        /// <summary>
+        /// Tries to load the assembly specified by the location argument and returns its assembly 
+        /// version.
+        /// </summary>
+        public static string GetAssemblyVersion(string location) {
+            var assembly = Assembly.ReflectionOnlyLoadFrom(location);
+            var assemblyVersion = assembly.GetName().Version.ToString();
+            return assemblyVersion;
+        }
+
+        /// <summary>
+        /// Gets the product version of the calling assembly when no type is passed as an argument. 
+        /// When a type is passed, that type's containing version is returned.
+        /// </summary>
+        public static string GetProductVersion(Type type = null) {
+            var assembly = type != null ? type.Assembly : Assembly.GetCallingAssembly();
+            return GetProductVersion(assembly.Location);
+        }
+
+        public static string GetProductVersion(string location) {
+            var assembly = Assembly.ReflectionOnlyLoadFrom(location);
+            var info = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return info.ProductVersion;
         }
     }
 }
--- test/VersionTools.Lib.Test/VersionHelperTest.cs
+++ test/VersionTools.Lib.Test/VersionHelperTest.cs
@@ -8,20 +8,29 @@ using Xunit;
 
 namespace VersionTools.Lib.Test {
     public class VersionHelperTest {
-        public class GetFileVersion_method {
+        public class GetAssemblyFileVersion_method {
             [Fact]
             public void should_return_the_full_file_version_string_of_the_calling_assembly() {
-                var fileVersion = VersionHelper.GetFileVersion();
+                var fileVersion = VersionHelper.GetAssemblyFileVersion();
                 Assert.Equal( "1.2.3-beta2+master.deadb33f", fileVersion );
             }
 
 
             [Fact]
             public void should_return_the_full_file_version_of_the_assembly_that_contains_the_specified_type() {
-                var fileVersion = VersionHelper.GetFileVersion(typeof (VersionHelper));
-                Assert.Equal("1.0.0.0", fileVersion);
+                var fileVersion = VersionHelper.GetAssemblyFileVersion(GetType());
+                Assert.Equal("1.2.3-beta2+master.deadb33f", fileVersion);
+            }
+
+
+            [Fact]
+            public void should_return_the_full_file_version_of_the_assembly_with_the_specified_path() {
+                var assemblyPath = Assembly.GetExecutingAssembly().Location;
+                var fileVersion = VersionHelper.GetAssemblyFileVersion(assemblyPath);
+                Assert.Equal( "1.2.3-beta2+master.deadb33f", fileVersion );
             }
         }
+
 
         public class GetAssemblyVersion_method {
             [Fact]
@@ -33,9 +42,43 @@ namespace VersionTools.Lib.Test {
 
             [Fact]
             public void should_return_the_assembly_version_of_the_assembly_containing_the_specified_type() {
-                var assemblyVersion = VersionHelper.GetAssemblyVersion(typeof (VersionHelper));
-                Assert.Equal("1.0.0.0", assemblyVersion);
+                var assemblyVersion = VersionHelper.GetAssemblyVersion(GetType());
+                Assert.Equal("1.2.3.0", assemblyVersion);
             }
+
+
+            [Fact]
+            public void should_return_the_assembly_version_of_the_assembly_at_the_specified_location() {
+                var assemblyPath = Assembly.GetExecutingAssembly().Location;
+                var assemblyVersion = VersionHelper.GetAssemblyVersion(assemblyPath);
+                Assert.Equal("1.2.3.0", assemblyVersion );
+            }
+
+        }
+
+
+        public class GetProductVersion_method {
+            [Fact]
+            public void should_return_the_product_version_of_the_calling_assembly() {
+                var productVersion = VersionHelper.GetProductVersion();
+                Assert.Equal("Test project v1.2.3", productVersion);
+            }
+
+
+            [Fact]
+            public void should_return_the_product_version_of_the_assembly_containing_the_specified_type() {
+                var productVersion = VersionHelper.GetProductVersion(typeof(VersionHelper));
+                Assert.Equal("1.0.0.0", productVersion);
+            }
+
+
+            [Fact]
+            public void should_return_the_product_version_of_the_assembly_at_the_specified_location() {
+                var assemblyPath = Assembly.GetExecutingAssembly().Location;
+                var productVersion = VersionHelper.GetProductVersion(assemblyPath);
+                Assert.Equal("Test project v1.2.3", productVersion);
+            }
+
 
         }
     }
