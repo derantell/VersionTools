f106b19487fde6b9d92fb4de47b7ad2572341b9e
David Tellander <dtellander@gmail.com>
2013-10-07 21:40:10 +0200

    Implemented equality and comparison operators
    - operator ==
    - operator !=
    - operator <
    - operator >
    - operator >=
    - operator <=

--- test/VersionTools.Lib.Test/SemverTest.cs
+++ test/VersionTools.Lib.Test/SemverTest.cs
@@ -174,14 +174,6 @@ namespace VersionTools.Lib.Test {
             }
 
             [Fact]
-            public void should_return_1_when_arg_is_null() {
-                var semver = Semver.Parse("1.2.3");
-                var compared = semver.CompareTo(null);
-
-                Assert.Equal(1, compared);
-            }
-
-            [Fact]
             public void should_return_minus_1_when_args_patch_nr_is_greater_than_own_patch_nr_other_things_equal() {
                 var v1 = Semver.Parse("1.2.1");
                 var v2 = Semver.Parse("1.2.2");
@@ -421,7 +413,147 @@ namespace VersionTools.Lib.Test {
                 Assert.False(areEqual);
                 
             }
+        }
 
+        public class operator_equals {
+            [Fact]
+            public void should_return_true_when_versions_are_equal() {
+                var ver1 = Semver.Parse("1.2.3-beta");
+                var ver2 = Semver.Parse("1.2.3-beta");
+
+                Assert.True( ver1 == ver2 );
+            }
+
+            [Fact]
+            public void should_return_false_when_versions_are_not_equal() {
+                var ver1 = Semver.Parse("1.2.3-beta");
+                var ver2 = Semver.Parse("1.2.3-charlie");
+
+                Assert.False( ver1 == ver2 );
+            }
+        }
+        
+        public class operator_not_equals {
+            [Fact]
+            public void should_return_false_when_versions_are_equal() {
+                var ver1 = Semver.Parse("1.2.3-beta");
+                var ver2 = Semver.Parse("1.2.3-beta");
+
+                Assert.False( ver1 != ver2 );
+            }
+
+            [Fact]
+            public void should_return_true_when_version_are_not_equal() {
+                var ver1 = Semver.Parse("1.2.3-beta");
+                var ver2 = Semver.Parse("1.2.3-charlie");
+
+                Assert.True( ver1 != ver2 );
+            }
+        }
+        
+        public class operator_greater_than {
+            [Fact]
+            public void should_return_true_when_left_operand_is_greater_than_right_operand() {
+                var ver1 = Semver.Parse("2.0.0");
+                var ver2 = Semver.Parse("1.9.9");
+
+                Assert.True( ver1 > ver2 );
+            }
+
+            [Fact]
+            public void should_return_false_when_left_operand_is_less_than_right_operand() {
+                var ver1 = Semver.Parse("2.0.0");
+                var ver2 = Semver.Parse("2.9.9");
+
+                Assert.False( ver1 > ver2 );
+            }
+
+            [Fact]
+            public void should_return_false_when_left_operand_is_equal_to_right_operand() {
+                var ver1 = Semver.Parse("2.0.0");
+                var ver2 = Semver.Parse("2.0.0");
+
+                Assert.False( ver1 > ver2 );
+            }
+        }
+
+        public class operator_less_than {
+            [Fact]
+            public void should_return_true_when_left_operand_is_less_than_right_operand() {
+                var ver1 = Semver.Parse("1.0.0");
+                var ver2 = Semver.Parse("1.9.9");
+
+                Assert.True( ver1 < ver2 );
+            }
+
+            [Fact]
+            public void should_return_false_when_left_operand_is_greater_than_right_operand() {
+                var ver1 = Semver.Parse("2.0.0");
+                var ver2 = Semver.Parse("1.9.9");
+
+                Assert.False( ver1 < ver2 );
+            }
+
+            [Fact]
+            public void should_return_false_when_left_operand_is_equal_to_right_operand() {
+                var ver1 = Semver.Parse("2.0.0-beta");
+                var ver2 = Semver.Parse("2.0.0-beta");
+
+                Assert.False( ver1 < ver2 );
+            }
+        }
+
+        public class operator_less_than_or_equal {
+            [Fact]
+            public void should_return_true_when_left_operand_is_less_than_right_operand() {
+                var ver1 = Semver.Parse("1.0.0");
+                var ver2 = Semver.Parse("1.9.9");
+
+                Assert.True( ver1 <= ver2 );
+            }
+
+            [Fact]
+            public void should_return_false_when_left_operand_is_greater_than_right_operand() {
+                var ver1 = Semver.Parse("2.0.0");
+                var ver2 = Semver.Parse("1.9.9");
+
+                Assert.False( ver1 <= ver2 );
+            }
+
+            [Fact]
+            public void should_return_true_when_left_operand_is_equal_to_right_operand() {
+                var ver1 = Semver.Parse("2.0.0-beta");
+                var ver2 = Semver.Parse("2.0.0-beta");
+
+                Assert.True( ver1 <= ver2 );
+            }
+        }
+
+        public class operator_greater_than_or_equal {
+            [Fact]
+            public void should_return_true_when_left_operand_is_greater_than_right_operand() {
+                var ver1 = Semver.Parse("2.0.0");
+                var ver2 = Semver.Parse("1.9.9");
+
+                Assert.True( ver1 >= ver2 );
+            }
+
+            [Fact]
+            public void should_return_false_when_left_operand_is_less_than_right_operand() {
+                var ver1 = Semver.Parse("2.0.0");
+                var ver2 = Semver.Parse("2.9.9");
+
+                Assert.False( ver1 >= ver2 );
+            }
+
+            [Fact]
+            public void should_return_true_when_left_operand_is_equal_to_right_operand() {
+                var ver1 = Semver.Parse("2.0.0");
+                var ver2 = Semver.Parse("2.0.0");
+
+                Assert.True( ver1 >= ver2 );
+            }
         }
     }
+
 }
